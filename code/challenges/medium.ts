1873e5e51d58807ae19b66dcba5c4351e32bf51a
Pineapple <1244476905@qq.com>
2021-11-30 23:57:24 +0800

    finished some medium chanllenges

--- code/challenges/medium.ts
+++ code/challenges/medium.ts
@@ -451,3 +451,33 @@ type IsUnion<T, F = T> = T extends T
     ? false
     : true
   : never;
+
+// 实现一个类型 ReplaceKeys，在联合类型中替换键，如果某个类型没有这个键，只需跳过替换，a 类型有三个参数。
+
+type NodeA = {
+  type: "A";
+  name: string;
+  flag: number;
+};
+
+type NodeB = {
+  type: "B";
+  id: number;
+  flag: number;
+};
+
+type NodeC = {
+  type: "C";
+  name: string;
+  flag: number;
+};
+
+type Nodes = NodeA | NodeB | NodeC;
+
+type ReplacedNodes = ReplaceKeys<
+  Nodes,
+  "name" | "flag",
+  { name: number; flag: string }
+>; // {type: 'A', name: number, flag: string} | {type: 'B', id: number, flag: string} | {type: 'C', name: number, flag: string} // would replace name from string to number, replace flag from number to string.
+
+type ReplacedNotExistKeys = ReplaceKeys<Nodes, "name", { aa: number }>; // {type: 'A', name: never, flag: number} | NodeB | {type: 'C', name: never, flag: number} // would replace name to never
